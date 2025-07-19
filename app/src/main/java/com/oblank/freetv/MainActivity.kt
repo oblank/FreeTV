5f58db86da7f7c7d5a18455be76af8ece200db6f
oBlank <dyh1919@gmail.com>
2025-07-19 12:39:24 +0800

    feat: update

--- app/src/main/java/com/oblank/freetv/MainActivity.kt
+++ app/src/main/java/com/oblank/freetv/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.oblank.freetv
 
-import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.FragmentActivity
 
-/**
- * Loads [MainFragment].
- */
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
         
-        // Launch WebViewActivity directly
-        val intent = Intent(this, WebViewActivity::class.java)
-        startActivity(intent)
-        finish()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.useWideViewPort = true
+        webView.settings.loadWithOverviewMode = true
+        
+        webView.webViewClient = WebViewClient()
+        
+        webView.loadUrl("https://www.55d4s8c8o6.shop/")
     }
 }
